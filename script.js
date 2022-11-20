b957c267e41c48bb3a33c888334dea26e3ceb268
Taha Yaseen <98612931+tahayaseenp@users.noreply.github.com>
2022-11-20 16:10:01 +0400

    Update script.js
    Functions to change background

---
+++ script.js
@@ -0,0 +1,15 @@
+function bg1() {
+    document.body.style.backgroundImage = "url('https://variety.com/wp-content/uploads/2021/07/Rick-Astley-Never-Gonna-Give-You-Up.png?w=681&h=383&crop=1')";
+  }  
+
+function bg2() {
+    document.body.style.backgroundImage = "url('https://variety.com/wp-content/uploads/2021/07/Rick-Astley-Never-Gonna-Give-You-Up.png?w=681&h=383&crop=1')";
+  }  
+
+function bg3() {
+    document.body.style.backgroundImage = "url('https://variety.com/wp-content/uploads/2021/07/Rick-Astley-Never-Gonna-Give-You-Up.png?w=681&h=383&crop=1')";
+  }  
+
+function bg4() {
+    document.body.style.backgroundImage = "url('https://variety.com/wp-content/uploads/2021/07/Rick-Astley-Never-Gonna-Give-You-Up.png?w=681&h=383&crop=1')";
+  }
