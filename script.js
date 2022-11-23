be7a41a63c402f879947a4d7810dc938755cd6eb
Taha Yaseen <98612931+tahayaseenp@users.noreply.github.com>
2022-11-23 07:58:02 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 // function bg1() {
-//     document.body.style.backgroundImage = "url('https://variety.com/wp-content/uploads/2021/07/Rick-Astley-Never-Gonna-Give-You-Up.png?w=681&h=383&crop=1')";
+//     document.body.style.backgroundImage = "url('https://hubblesite.org/files/live/sites/hubble/files/home/resource-gallery/articles/_images/hs-article-bigBang-2400x1200.jpg')";
 //   }  
 
 // function bg2() {
-//     document.body.style.backgroundImage = "url('https://variety.com/wp-content/uploads/2021/07/Rick-Astley-Never-Gonna-Give-You-Up.png?w=681&h=383&crop=1')";
+//     document.body.style.backgroundImage = "url('https://i.natgeofe.com/k/a2a738a9-e019-4911-98e6-17f31c45ac88/milky-way-2_3x2.jpg')";
 //   }  
 
 // function bg3() {
-//     document.body.style.backgroundImage = "url('https://variety.com/wp-content/uploads/2021/07/Rick-Astley-Never-Gonna-Give-You-Up.png?w=681&h=383&crop=1')";
+//     document.body.style.backgroundImage = "url('https://eapsweb.mit.edu/sites/default/files/styles/700x347_no_overlay/public/fig16.jpg__1240x510_q85_subject_location-375%2C250_subsampling-2.jpg?itok=qbw9k4rk')";
 //   }  
 
 // function bg4() {
