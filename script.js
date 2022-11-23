649d5a4e3b0cac25d2c352344eeb47bbc8068b59
Taha Yaseen <98612931+tahayaseenp@users.noreply.github.com>
2022-11-23 08:05:25 +0400

    Update script.js

--- script.js
+++ script.js
@@ -3,19 +3,13 @@ function bg1() {
    }  
 
 function bg2() {
-
   document.body.style.backgroundImage = "url('https://i.natgeofe.com/k/a2a738a9-e019-4911-98e6-17f31c45ac88/milky-way-2_3x2.jpg')";
-
-}  
+   }  
 
 function bg3() {
-
   document.body.style.backgroundImage = "url('https://eapsweb.mit.edu/sites/default/files/styles/700x347_no_overlay/public/fig16.jpg__1240x510_q85_subject_location-375%2C250_subsampling-2.jpg?itok=qbw9k4rk')";
-
-}  
+  }  
 
 function bg4() {
-
   document.body.style.backgroundImage = "url('https://variety.com/wp-content/uploads/2021/07/Rick-Astley-Never-Gonna-Give-You-Up.png?w=681&h=383&crop=1')";
-
-}
+ }
