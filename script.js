5d715f0ab91309db94f830619b732c6613591bce
KrishnanNithin <75091510+KrishnanNithin@users.noreply.github.com>
2022-11-28 01:15:47 +0400

    Added core JS and CSS functionality

--- script.js
+++ script.js
@@ -1,15 +1,50 @@
-function bg1() {
-     document.body.style.backgroundImage = "url('https://hubblesite.org/files/live/sites/hubble/files/home/resource-gallery/articles/_images/hs-article-bigBang-2400x1200.jpg')";
-   }  
+const body = document.querySelector('body');
 
+function bg1() {  
+  body.classList.remove(...body.classList);
+  body.classList.add("bg1");
+   }  
 function bg2() {
-  document.body.style.backgroundImage = "url('https://i.natgeofe.com/k/a2a738a9-e019-4911-98e6-17f31c45ac88/milky-way-2_3x2.jpg')";
+  body.classList.remove(...body.classList);
+  body.classList.add("bg2");
    }  
-
 function bg3() {
-  document.body.style.backgroundImage = "url('https://media-cldnry.s-nbcnews.com/image/upload/t_fit-1500w,f_auto,q_auto:best/MSNBC/Components/Photo/_new/121001_ShockWavePhoto-0110p_files.jpg')";
+  body.classList.remove(...body.classList);
+  body.classList.add("bg3");
   }  
-
 function bg4() {
-  document.body.style.backgroundImage = "url('https://variety.com/wp-content/uploads/2021/07/Rick-Astley-Never-Gonna-Give-You-Up.png?w=681&h=383&crop=1')";
- }
+  body.classList.remove(...body.classList);
+  body.classList.add("bg4");
+}
+function bg5() {
+  body.classList.remove(...body.classList);
+  body.classList.add("bg5");
+}
+function bg6() {
+  body.classList.remove(...body.classList);
+  body.classList.add("bg6");
+}
+function bg7() {
+  body.classList.remove(...body.classList);
+  body.classList.add("bg7");
+}
+function bg8() {
+  body.classList.remove(...body.classList);
+  body.classList.add("bg8");
+}
+function bg9() {
+  body.classList.remove(...body.classList);
+  body.classList.add("bg9");
+}
+function bg10() {
+  body.classList.remove(...body.classList);
+  body.classList.add("bg10");
+}
+function bg11() {
+  body.classList.remove(...body.classList);
+  body.classList.add("bg11");
+}
+function bg12() {
+  body.classList.remove(...body.classList);
+  body.classList.add("bg12");
+}
