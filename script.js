3f261f8112b5b71758fadd2b4c19fc779bc3b247
Taha Yaseen <98612931+tahayaseenp@users.noreply.github.com>
2022-11-28 12:39:51 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,54 +1,65 @@
 const body = document.querySelector('body');
 
-function bgreset(){
-  body.classList.remove(...body.classList);
+function bgreset() {
+    body.classList.remove(...body.classList);
 }
 
 function bg1() {  
-  body.classList.remove(...body.classList);
-  body.classList.add("bg1");
-   }  
+    body.classList.remove(...body.classList);
+    body.classList.add("bg1");
+}  
+
 function bg2() {
-  body.classList.remove(...body.classList);
-  body.classList.add("bg2");
-   }  
+    body.classList.remove(...body.classList);
+    body.classList.add("bg2");
+}
+
 function bg3() {
-  body.classList.remove(...body.classList);
-  body.classList.add("bg3");
-  }  
+    body.classList.remove(...body.classList);
+    body.classList.add("bg3");
+}  
+
 function bg4() {
-  body.classList.remove(...body.classList);
-  body.classList.add("bg4");
+    body.classList.remove(...body.classList);
+    body.classList.add("bg4");
 }
+
 function bg5() {
-  body.classList.remove(...body.classList);
-  body.classList.add("bg5");
+    body.classList.remove(...body.classList);
+    body.classList.add("bg5");
 }
+
 function bg6() {
-  body.classList.remove(...body.classList);
-  body.classList.add("bg6");
+    body.classList.remove(...body.classList);
+    body.classList.add("bg6");
 }
+
 function bg7() {
-  body.classList.remove(...body.classList);
-  body.classList.add("bg7");
+    body.classList.remove(...body.classList);
+    body.classList.add("bg7");
 }
+
 function bg8() {
-  body.classList.remove(...body.classList);
-  body.classList.add("bg8");
+    body.classList.remove(...body.classList);
+    body.classList.add("bg8");
 }
+
 function bg9() {
-  body.classList.remove(...body.classList);
-  body.classList.add("bg9");
+    body.classList.remove(...body.classList);
+    body.classList.add("bg9");
 }
+
 function bg10() {
-  body.classList.remove(...body.classList);
-  body.classList.add("bg10");
+    body.classList.remove(...body.classList);
+    body.classList.add("bg10");
 }
+
 function bg11() {
-  body.classList.remove(...body.classList);
-  body.classList.add("bg11");
+    body.classList.remove(...body.classList);
+    body.classList.add("bg11");
 }
+
 function bg12() {
-  body.classList.remove(...body.classList);
-  body.classList.add("bg12");
+    body.classList.remove(...body.classList);
+    body.classList.add("bg12");
 }
